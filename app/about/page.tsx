34f8affb15f3c712a83c089bf449a4767326b147
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2026-08-13 22:52:19 +0800

    Simplify about page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,36 +1,21 @@
 import { Header } from "@/components/Header";
-import { getProducts, getSiteContent } from "@/lib/cms";
+import { getSiteContent } from "@/lib/cms";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function AboutPage() {
-  const [site, products] = await Promise.all([getSiteContent(), getProducts()]);
-  const categories = Array.from(new Set(products.map((product) => product.category).filter(Boolean)));
+  const site = await getSiteContent();
 
   return (
     <div className="shell">
       <Header site={site} />
       <main className="main">
         <section className="content-page about-page">
-          <div>
+          <div className="about-content">
             <span className="eyebrow">关于我们</span>
             <h1>{site.aboutTitle}</h1>
             <p>{site.aboutDescription}</p>
-            <div className="content-stats">
-              <div>
-                <b>{products.length}</b>
-                <span>产品条目</span>
-              </div>
-              <div>
-                <b>{categories.length}</b>
-                <span>产品分类</span>
-              </div>
-              <div>
-                <b>KASONS</b>
-                <span>品牌服务</span>
-              </div>
-            </div>
           </div>
 
           {site.aboutQrImage && (
